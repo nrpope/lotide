badde27388c255725e42f992de955d6ce1ee8e88
Nick Pope <psydpope@gmail.com>
2020-02-05 21:04:04 +0000

    inital commit

---
+++ letterPositions.js
@@ -0,0 +1,27 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const eqArrays = function(array1, array2) {
+  if (array1.length !== array2.length) {
+    return false;
+  }
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+//end of eqArray and assertEqual functions
+
+const letterPositions = function(sentence) {
+  const results = {};
+  // logic to update results here
+  let index = 0;
+  return results;
+};
