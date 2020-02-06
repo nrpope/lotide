5883dc98b8bec8002a3d0031b256b508c5e49722
Nick Pope <psydpope@gmail.com>
2020-02-06 23:04:06 +0000

    initial commit + light refactoring

---
+++ takeUntil.js
@@ -0,0 +1,56 @@
+const eqArrays = function(array1, array2) {
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const assertArraysEqual = function(actual, expected) {
+  if (eqArrays(actual, expected) === true) {
+    console.log(`✅✅✅ Assertion Passed: "${actual}" === "${expected}"`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: "${actual}" !== "${expected}"`);
+  }
+};
+const takeUntil = (array, callback) => {
+  let newArray = [];
+  for (let element of array) {
+    if (!callback(element)) {
+      newArray.push(element);
+    } else {
+      break;
+    }
+  }
+  return newArray;
+};
+
+const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
+const results1 = takeUntil(data1, x => x < 0);
+console.log(results1);
+
+console.log("---");
+
+const data2 = [
+  "I've",
+  "been",
+  "to",
+  "Hollywood",
+  ",",
+  "I've",
+  "been",
+  "to",
+  "Redwood"
+];
+const results2 = takeUntil(data2, x => x === ",");
+console.log(results2);
+
+/*expected outcome
+[ 1, 2, 5, 7, 2 ]
+---
+[ 'I\'ve', 'been', 'to', 'Hollywood' ]
+*/
+
+assertArraysEqual(results1, data1.slice(0, 5));
+assertArraysEqual(results2, data2.slice(0, 4));
